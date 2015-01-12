94956ab9615fb9c102ebc63d8082860c6ba59033
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2015-01-12 08:52:38 -0500

    increase video size to 320x240px. and increase message size limit

--- src/main/java/instagoogly/WebSocketConfig.java
+++ src/main/java/instagoogly/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,4 +21,9 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/cameraIn").withSockJS();
 	}
+	
+	@Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(256 * 1024);
+    }
 }
